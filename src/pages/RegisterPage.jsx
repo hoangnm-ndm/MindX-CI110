53fdb8a415008df8f0bf273ba268f6892bdc462a
hoangnm-ndm <hoangnm.ndm@gmail.com>
2026-02-27 21:29:40 +0700

    Day 04: State

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,39 +1,87 @@
+import Password from "antd/es/input/Password";
 import React, { useState } from "react";
 
 const RegisterPage = () => {
-  const [email, setEmail] = useState("");
-  const handleSubmit = (data) => {
-    console.log(data);
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+    confirmPass: "",
+    gender: "",
+  });
+
+  const hanleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
   };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // * Gửi dữ liệu lên server để đăng ký! (Gọi API)
+    console.log(form);
+  };
+
+  //* Hãy thêm tính năng kiểm tra mật khẩu phải có tổi thiểu 6 ký tự.
+  //* Password === confirmPass
+
   return (
-    <form action="" onSubmit={() => handleSubmit(data)}>
+    <form action="" onSubmit={handleSubmit}>
       <h1>Register now!</h1>
       <div className="mb-3">
         <label className="form-label">Email</label>
         <input
-          type="text"
+          type="email"
           className="form-control"
-          onChange={console.log("test")}
+          name="email"
+          value={form.email}
+          onChange={hanleChange}
+          required
         />
       </div>
 
       <div className="mb-3">
         <label className="form-label">Password</label>
-        <input type="password" className="form-control" />
+        <input
+          type="password"
+          className="form-control"
+          name="password"
+          value={form.password}
+          onChange={hanleChange}
+          required
+        />
       </div>
 
       <div className="mb-3">
         <label className="form-label">Confirm Password</label>
-        <input type="password" className="form-control" />
+        <input
+          type="password"
+          className="form-control"
+          name="confirmPass"
+          value={form.confirmPass}
+          onChange={hanleChange}
+          required
+        />
       </div>
 
       <div className="mb-3">
         <label htmlFor="">Gender</label>
-        <select name="" id="" className="form-control">
-          <option value="">Choose dender</option>
-          <option value="">Male</option>
-          <option value="">Female</option>
-          <option value="">Other</option>
+        <select
+          id=""
+          className="form-control"
+          name="gender"
+          value={form.gender}
+          onChange={hanleChange}
+        >
+          <option key={1} value="">
+            Choose gender
+          </option>
+          <option key={2} value="male">
+            Male
+          </option>
+          <option key={3} value="female">
+            Female
+          </option>
+          <option key={4} value="other">
+            Other
+          </option>
         </select>
       </div>
 
