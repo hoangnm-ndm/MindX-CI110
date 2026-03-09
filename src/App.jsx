15503fd792d57cc5b66c388cf4448061189757a3
hoangnm-ndm <hoangnm.ndm@gmail.com>
2026-03-09 20:21:36 +0700

    Day 07: Call API with useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,51 @@
 import React, { useEffect, useState } from "react";
 
+/**
+ * * useEffect(callback, dep)
+ * * 1. dep = null (không khai báo): callback được gọi mỗi lần component render
+ * * 2. dep = [] (array rỗng): callback chạy 1 lần duy nhất ngay sau khi component mounted.
+ * * 3. dep = [deps]: callback được gọi lần đầu sau khi component render, và được gọi lại mỗi lần khi deps có sự thay đổi.
+ * * 4. dùng useEffect để clean-up
+ */
+
 function App() {
-  const [count, setCount] = useState(0);
-  const [otherState, setOtherState] = useState(1000);
-  console.log(count, otherState);
-  // Sử dụng useEffect
+  const [products, setProducts] = useState([]);
+  const [skip, setSkip] = useState(0);
   useEffect(() => {
-    document.title = "Tieu De: " + count;
-    // console.log("làm đúng 1 lần, vì không phụ thuộc vào ai");
-    // * Tiêu đề nó không thay đổi vì code trong callback của useEffect chỉ phụ thuộc vào count
-  }, []);
-
-  /**
-   * * useEffect(callback, dep)
-   * * 1. dep = null (không khai báo): callback được gọi mỗi lần component render
-   * * 2. dep = [] (array rỗng): callback chạy 1 lần duy nhất ngay sau khi component mounted.
-   * * 3. dep = [deps]: callback được gọi lần đầu sau khi component render, và được gọi lại mỗi lần khi deps có sự thay đổi.
-   * * 4. dùng useEffect để clean-up
-   */
-
+    fetch(`https://dummyjson.com/products?limit=4&skip=${skip}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.products);
+        setProducts(data.products);
+      });
+  }, [skip]);
   return (
     <div>
-      <button onClick={() => setCount(count + 1)}>Click me</button>
+      <h1>Danh sach san pham HOT thang 3</h1>
+      <div>
+        {products.map((item, index) => (
+          <div key={item.id} className="border-slate-600 border-2 p-4">
+            <h2>{item.title}</h2>
+            <p>Gia: {item.price}</p>
+            <img src={item.thumbnail} alt="" />
+            <button>Mua ngay</button>
+          </div>
+        ))}
+      </div>
+
+      <button>prev</button>
       <button
-        className="border-slate-500 border-2"
-        onClick={() => setOtherState(otherState + 100)}
+        className="border-2 px-4 py-2 bg-amber-500"
+        onClick={() => {
+          setSkip(skip + 4);
+        }}
       >
-        Click me
+        next
       </button>
     </div>
   );
 }
+
+//* Dùng axios thay cho fetch...
 
 export default App;
