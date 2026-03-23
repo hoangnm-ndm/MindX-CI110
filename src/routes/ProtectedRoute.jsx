22fa29912df54471a25530e2041700aa00307636
hoangnm-ndm <hoangnm.ndm@gmail.com>
2026-03-23 19:14:20 +0700

    Day 10: Config tailwind

--- src/routes/ProtectedRoute.jsx
+++ src/routes/ProtectedRoute.jsx
@@ -2,7 +2,8 @@ import React from "react";
 
 const ProtectedRoute = ({ children, roles }) => {
   const user = JSON.parse(localStorage.getItem("user"));
-
+  // sử dụng state để lưu và lấy thông tin từ localStorage sẽ giúp chúng ta dễ dàng quản lý và cập nhật thông tin người dùng khi cần thiết, đặc biệt là khi có sự thay đổi về quyền truy cập hoặc khi người dùng đăng xuất.
+  //  * Nên đào sâu thêm về cookies, sessionStorage, localStorage để hiểu rõ hơn về cách lưu trữ thông tin người dùng và token
   const checkRole = roles.includes(user?.role);
   console.log(checkRole);
   return (
