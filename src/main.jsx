45c1ef0dff6e767cd7ee8e9186838bb115272a6f
hoangnm-ndm <hoangnm.ndm@gmail.com>
2026-04-03 19:15:09 +0700

    delete import testMemo in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router";
 import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter } from "react-router";
-import TestUseRef from "./pages/TestUseRef.jsx";
-import TestMemo from "./pages/testMemo.jsx";
-import TestUseMemo from "./pages/TestUseMemo.jsx";
-import TestUseCallback from "./pages/TestUseCallback.jsx";
-import CounterComponent from "./pages/CounterComponent.jsx";
-import ShopPage from "./pages/ShopPage.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
-      {/* <TestUseRef /> */}
-      {/* <TestMemo /> */}
-      {/* <TestUseMemo /> */}
-      {/* <TestUseCallback /> */}
-      {/* <CounterComponent /> */}
-      {/* <ShopPage /> */}
     </BrowserRouter>
   </React.StrictMode>
 );
